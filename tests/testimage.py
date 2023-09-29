2308b14b428608c12717c9e9f3153458906c7a4e
Pierre-Yves HERVE <pyherve@fealinx.com>
2023-09-29 09:20:32 +0000

    formatting

--- tests/testimage.py
+++ tests/testimage.py
@@ -1,6 +1,7 @@
-"""Unit tests for SHIVA preprocessing tools
-"""
+"""Unit tests for SHIVA preprocessing tools."""
+
 import unittest
+
 import nibabel as nb
 import nibabel.processing as nip
 
@@ -8,19 +9,17 @@ from shivautils.image import normalization, thresholding, crop
 
 
 class TestImage(unittest.TestCase):
-    """Test function of module image."""
+    """Test image module functionalities."""
     def setUp(self):
         self.img = nb.loadsave.load("C:/scratch/raw/01.nii")
 
     def test_normalize_run(self):
-        """Check if value max of normalized image is less than img input
-        """
+        """Check if value max of normalized image is less than img input."""
         self.assertLessEqual(normalization(self.img).get_fdata().max(),
                              self.img.get_fdata().max())
 
     def test_crop_modified_shape(self):
-        """Check modification of shape's image
-        """
+        """Check modification of image shape"""
         resampled = nip.conform(self.img, out_shape=(256, 256, 256),
                                 voxel_size=(1, 1, 1), order=3,
                                 cval=0.0, orientation='RAS',
@@ -32,8 +31,7 @@ class TestImage(unittest.TestCase):
         self.assertEqual((160, 214, 176), img_cropped.shape)
 
     def test_threshold_run(self):
-        """Check if value max of thresholding image is less than img input
-        """
+        """Check if value max of thresholding image is less than img input."""
         self.assertLessEqual(thresholding(self.img).get_fdata().mean(),
                              self.img.get_fdata().mean())
 
