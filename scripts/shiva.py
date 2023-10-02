c36363fdd913bea5942867b54ebeed56238f1841
Victor Nozais <vnozais@fealinx.com>
2023-10-02 10:12:48 +0200

    Corrected uppercase

--- scripts/shiva.py
+++ scripts/shiva.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 """Workflow script for singularity container"""
-from shivautils.workflows.swi_postprocessing import genWorkflow as genWorkflowPostSWI
-from shivautils.workflows.swi_predict import genWorkflow as genWorkflowPredictSWI
-from shivautils.workflows.swi_preprocessing import genWorkflow as genWorkflowSWI
+from shivautils.workflows.SWI_postprocessing import genWorkflow as genWorkflowPostSWI
+from shivautils.workflows.SWI_predict import genWorkflow as genWorkflowPredictSWI
+from shivautils.workflows.SWI_preprocessing import genWorkflow as genWorkflowSWI
 from shivautils.workflows.dual_post_processing_container import genWorkflow as genWorkflowPost
 from shivautils.workflows.dual_predict import genWorkflow as genWorkflowPredict
 from shivautils.workflows.dual_preprocessing import genWorkflow
