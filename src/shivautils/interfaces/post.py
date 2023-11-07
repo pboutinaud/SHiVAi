5579ebaf1611526ea908305f05742e0d1b3b0f45
Victor Nozais <vnozais@fealinx.com>
2023-11-07 12:17:06 +0100

    New interface for QC metrics node (plus AutoPEP)

--- src/shivautils/interfaces/post.py
+++ src/shivautils/interfaces/post.py
@@ -8,17 +8,21 @@ import os
 import os.path as op
 
 import numpy as np
+import nibabel as nib
+import pandas as pd
 
 from nipype.interfaces.base import (traits, File, TraitedSpec,
                                     BaseInterface, BaseInterfaceInputSpec,
-                                    CommandLine, CommandLineInputSpec, 
-                                    InputMultiPath, OutputMultiPath) 
+                                    CommandLine, CommandLineInputSpec,
+                                    InputMultiPath, OutputMultiPath)
 from nipype.interfaces.spm.base import (SPMCommand, SPMCommandInputSpec)
 
 from nipype.interfaces.matlab import MatlabCommand
 from nipype.utils.filemanip import ensure_list, simplify_list
 
 from string import Template
+from shivautils.stats import transf_from_affine
+
 
 class CustomIntensityNormalizationInputSpec(BaseInterfaceInputSpec):
     in_file = File(exists=True, mandatory=True)
@@ -30,7 +34,6 @@ class CustomIntensityNormalizationInputSpec(BaseInterfaceInputSpec):
 
 class CustomIntensityNormalizationOutputSpec(TraitedSpec):
     out_file = File(exists=True)
-
 
 
 class CustomIntensityNormalization(BaseInterface):
@@ -60,9 +63,10 @@ class CustomIntensityNormalization(BaseInterface):
         outputs['out_file'] = op.abspath(self.inputs.out_file)
         return outputs
 
+
 class MaskOverlayQCplotInputSpec(CommandLineInputSpec):
     bg_im_file = traits.Str(mandatory=True,
-                            desc ='Background ref image file (typically T1 brain)',
+                            desc='Background ref image file (typically T1 brain)',
                             argstr='%s',
                             position=1)
     mask_file = traits.Str(mandatory=True,
@@ -70,39 +74,40 @@ class MaskOverlayQCplotInputSpec(CommandLineInputSpec):
                            argstr='%s',
                            position=2)
     transparency = traits.Enum(0, 1,
-                              argstr='%d',
-                              desc='Set transparency (0: solid, 1:transparent)',
-                              mandatory=True,
-                              position=3)
+                               argstr='%d',
+                               desc='Set transparency (0: solid, 1:transparent)',
+                               mandatory=True,
+                               position=3)
     out_file = traits.Str('mask_overlay.png',
-                           mandatory=False,
-                           desc='Output png filename',
-                           argstr='%s',
-                           position=4)
+                          mandatory=False,
+                          desc='Output png filename',
+                          argstr='%s',
+                          position=4)
     bg_max = traits.Float(argstr="%.3f",
                           mandatory=False,
                           desc='Optionally specifies the bg img intensity range as a percentile',
                           position=5)
+
 
 class MaskOverlayQCplotOutputSpec(TraitedSpec):
     out_file = File(exists=True)
 
 
 class MakeDistanceMapInputSpec(CommandLineInputSpec):
-    
+
     # niimath ventricle_mask  -binv -edt output
     in_file = traits.Str(mandatory=True,
-                         desc ='Object segmentation mask (isotropic)' ,
+                         desc='Object segmentation mask (isotropic)',
                          argstr='%s',
                          position=1)
-    
+
     out_file = traits.Str('distance_map.nii.gz',
-                           mandatory=True,
-                           desc='Output filename for ventricle distance maps',
-                           argstr='-binv -edt %s',
-                           position=2)
-    
-    
+                          mandatory=True,
+                          desc='Output filename for ventricle distance maps',
+                          argstr='-binv -edt %s',
+                          position=2)
+
+
 class MakeDistanceMapOutputSpec(TraitedSpec):
     out_file = File(exists=True)
 
@@ -112,52 +117,50 @@ class MakeDistanceMap(CommandLine):
 
     _cmd = 'niimath'
 
-    input_spec =  MakeDistanceMapInputSpec
+    input_spec = MakeDistanceMapInputSpec
     output_spec = MakeDistanceMapOutputSpec
 
     def _list_outputs(self):
         outputs = self.output_spec().get()
         outputs['out_file'] = op.abspath(self.inputs.out_file)
         return outputs
- 
-  
+
+
 class SynthSegSegmentationInputSpec(CommandLineInputSpec):
-    
+
     # like: os.system("mri_synthseg --i /data/path --o /save/dir --vol /save/dir --qc /save/dir")
-    
+
     im_file = traits.Str(mandatory=True,
-                            desc ='Path to MRI image / path to folder with MRI images',
-                            argstr='%s',
-                            position=1)
-    
+                         desc='Path to MRI image / path to folder with MRI images',
+                         argstr='%s',
+                         position=1)
+
     out_file = traits.Str('SynthSegSegmentation_111.nii.gz',
-    
-                           mandatory=True,
-                           desc='Output SynthSeg map',
-                           argstr='%s',
-                           position=2)
-    
+
+                          mandatory=True,
+                          desc='Output SynthSeg map',
+                          argstr='%s',
+                          position=2)
+
     out_vol = traits.Str('vol.csv',
-    
-                           mandatory=True,
-                           desc='Output SynthSeg map',
-                           argstr='%s',
-                           position=3)
-    
-    
+
+                         mandatory=True,
+                         desc='Output SynthSeg map',
+                         argstr='%s',
+                         position=3)
+
     out_qc = traits.Str('qc.csv',
-                           mandatory=True,
-                           desc='Output SynthSeg map',
-                           argstr='%s',
-                           position=4)
-   
-    
- 
+                        mandatory=True,
+                        desc='Output SynthSeg map',
+                        argstr='%s',
+                        position=4)
+
+
 class SynthSegSegmentationOutputSpec(TraitedSpec):
     out_file = File(exists=True)
     out_qc = File(exists=True)
     out_vol = File(exists=True)
-    
+
 
 class SynthSegSegmentationMap(CommandLine):
     """
@@ -166,7 +169,7 @@ class SynthSegSegmentationMap(CommandLine):
     import shivautils.interfaces as wf_interfaces
     p = op.dirname(wf_interfaces.__file__)
     _cmd = op.join(p, 'SynthSegSegmentation.sh')
-    input_spec =  SynthSegSegmentationInputSpec
+    input_spec = SynthSegSegmentationInputSpec
     output_spec = SynthSegSegmentationOutputSpec
 
     def _list_outputs(self):
@@ -176,55 +179,55 @@ class SynthSegSegmentationMap(CommandLine):
         outputs['out_qc'] = op.abspath(self.inputs.out_qc)
 
         return outputs
- 
-
 
 
 class CoregQCInputSpec(CommandLineInputSpec):
-    
+
     """Create coregistrations qc endpoints:
 
      1) create coreg isocontour image
      2) compute the cost function of FLIRT coregistration
-     
+
      coreg_QC.sh <COREGISTERED_IMG> <REF(T1)_BRAIN> <REF(T1)_BRAIN_MASK> <OUTPUT_BASENAME>
-    
+
      creates <OUTPUT_BASENAME>.png with isocontours image and <OUTPUT_BASENAME>.txt
      with costfunction value
     """
-    
+
     in_file = traits.Str(mandatory=True,
-                            desc ='Path to coregistered image',
-                            argstr='%s',
-                            position=1)
-    
+                         desc='Path to coregistered image',
+                         argstr='%s',
+                         position=1)
+
     ref_file = traits.Str(
-                           mandatory=True,
-                           desc='Path to reference image (T1w)',
-                           argstr='%s',
-                           position=2)
-    
+        mandatory=True,
+        desc='Path to reference image (T1w)',
+        argstr='%s',
+        position=2)
+
     ref_mat = traits.Str(mandatory=True,
-                           desc='path to a reference matrix',
-                           argstr='%s',
-                           position=3)
-    
-    out_txt = traits.Str(  'cost_function.txt',
-                           mandatory=False,
-                           desc='Output txt filename',
-                           argstr='%s',
-                           position=4)
-    
-    out_png = traits.Str(  'isocontour_image.png',
-                           mandatory=False,
-                           desc='Output  png filename',
-                           argstr='%s',
-                           position=5)
-    
+                         desc='path to a reference matrix',
+                         argstr='%s',
+                         position=3)
+
+    out_txt = traits.Str('cost_function.txt',
+                         mandatory=False,
+                         desc='Output txt filename',
+                         argstr='%s',
+                         position=4)
+
+    out_png = traits.Str('isocontour_image.png',
+                         mandatory=False,
+                         desc='Output  png filename',
+                         argstr='%s',
+                         position=5)
+
+
 class coregQCOutputSpec(TraitedSpec):
 
     out_txt = File(exists=True)
     out_png = File(exists=True)
+
 
 class coregQC(CommandLine):
     """
@@ -233,17 +236,16 @@ class coregQC(CommandLine):
     import shivautils.interfaces as wf_interfaces
     p = op.dirname(wf_interfaces.__file__)
     _cmd = op.join(p, 'coreg_QC.sh')
-    input_spec =  CoregQCInputSpec
+    input_spec = CoregQCInputSpec
     output_spec = coregQCOutputSpec
 
     def _list_outputs(self):
         outputs = self.output_spec().get()
-        outputs['out_txt'] = op.abspath(self.inputs.out_txt)     
-        outputs['out_png'] = op.abspath(self.inputs.out_png)          
+        outputs['out_txt'] = op.abspath(self.inputs.out_txt)
+        outputs['out_png'] = op.abspath(self.inputs.out_png)
         return outputs
 
- 
-  
+
 class MaskOverlayQCplot(CommandLine):
     """
     Creates multi-slice axial plot with Slicer showing mask overlaied on
@@ -260,6 +262,7 @@ class MaskOverlayQCplot(CommandLine):
         outputs['out_file'] = op.abspath(self.inputs.out_file)
         return outputs
 
+
 class SPMApplyDeformationInput(SPMCommandInputSpec):
     in_files = InputMultiPath(
         File(exists=True),
@@ -267,7 +270,7 @@ class SPMApplyDeformationInput(SPMCommandInputSpec):
         field="out{1}.pull.fnames",
         desc="Files on which deformation is applied",
     )
-    
+
     deformation_field = File(
         exists=True,
         mandatory=True,
@@ -275,7 +278,7 @@ class SPMApplyDeformationInput(SPMCommandInputSpec):
         desc="SPM deformation file"
     )
     target = File(
-        exists=True, 
+        exists=True,
         mandatory=True,
         field="comp{2}.id.space",
         desc="File defining target space"
@@ -312,6 +315,7 @@ class SPMApplyDeformation(SPMCommand):
     output_spec = SPMApplyDeformationOutput
     _jobtype = "util"
     _jobname = "defs"
+
     def _format_arg(self, opt, spec, val):
         """Convert input to appropriate format for spm"""
         if opt == "in_files":
@@ -321,10 +325,85 @@ class SPMApplyDeformation(SPMCommand):
         if opt == "deformation_field":
             return np.array([simplify_list(val)], dtype=object)
         return val
+
     def _list_outputs(self):
         outputs = self._outputs().get()
         outputs["out_files"] = []
         for filename in self.inputs.in_files:
             _, fname = os.path.split(filename)
             outputs["out_files"].append(op.realpath("w%s" % fname))
+        return outputs
+
+
+class QC_metrics_Input(BaseInterfaceInputSpec):
+
+    brain_mask = traits.File(
+        exists=True,
+        mandatory=True,
+        desc="Brain mask nifti file"
+    )
+
+    main_norm_peak = traits.Float(
+        exists=True,
+        mandatory=True,
+        desc="Peak from the histogram of the 'main' iamge (t1 or swi)"
+    )
+
+    flair_reg_mat = traits.File(
+        None,
+        usedefault=True,
+        exists=True,
+        mandatory=False,
+        desc="Affine matrix (.mat) of registration between flair and t1"
+    )
+
+    swi_reg_mat = traits.File(
+        None,
+        usedefault=True,
+        exists=True,
+        mandatory=False,
+        desc="Affine matrix (.mat) of registration between swi and t1"
+    )
+
+
+class QC_metrics_Output(TraitedSpec):
+    csv_qc_metrics = traits.File(exists=True,
+                                 desc="csv containing the subject's qc metrics")
+
+
+class QC_metrics(BaseInterface):
+    """
+    Compute and gather the quality control metrics for the corrent subject
+    This can be used to automatically detect subject with bad preprocessing
+    """
+    input_spec = QC_metrics_Input
+    output_spec = QC_metrics_Output
+
+    def _run_interface(self, runtime):
+        qc_dict = {}
+        brain_vol = nib.load(self.input_spec.brain_mask).get_fdata().astype(bool)
+        brain_size = brain_vol.sum()
+        qc_dict['brain_mask_size'] = brain_size
+        qc_dict['main_im_hist_peak'] = self.input_spec.main_norm_peak
+
+        if self.input_spec.flair_reg_mat is not None:
+            rotation_flair_reg, translation_flair_reg = transf_from_affine(self.input_spec.flair_reg_mat)
+            qc_dict['rotation_flair_reg'] = rotation_flair_reg
+            qc_dict['translation_flair_reg'] = translation_flair_reg
+
+        if self.input_spec.swi_reg_mat is not None:
+            rotation_swi_reg, translation_swi_reg = transf_from_affine(self.input_spec.swi_reg_mat)
+            qc_dict['rotation_swi_reg'] = rotation_swi_reg
+            qc_dict['translation_swi_reg'] = translation_swi_reg
+
+        qc_df = pd.DataFrame(qc_dict)
+        qc_file = 'qc_metrics.csv'
+        qc_df.to_csv(qc_file)
+
+        setattr(self, 'csv_qc_metrics', os.path.abspath(qc_file))
+        return runtime
+
+    def _list_outputs(self):
+        outputs = self.output_spec().get()
+        outputs['csv_qc_metrics'] = getattr(self, 'csv_qc_metrics')
         return outputs
