46a0f2e1a31d2ec48d7a24c5f2e00f86980ad511
Victor Nozais <vnozais@fealinx.com>
2023-11-10 14:28:45 +0100

    make it compatible with other workflows

--- src/shivautils/workflows/preprocessing_premasked.py
+++ src/shivautils/workflows/preprocessing_premasked.py
@@ -12,7 +12,7 @@ from nipype.pipeline.engine import Node, Workflow
 from nipype.interfaces import ants
 from nipype.interfaces.io import DataGrabber
 
-from shivautils.interfaces.image import (Normalization,
+from shivautils.interfaces.image import (Normalization, Threshold,
                                          Conform, Crop, Resample_from_to)
 
 
@@ -74,6 +74,12 @@ def genWorkflow(**kwargs) -> Workflow:
     mask_to_crop.inputs.spline_order = 0  # should be equivalent to NearestNeighbor(?)
     workflow.connect(conform_mask, 'resampled', mask_to_crop, 'moving_image')
     workflow.connect(crop, "cropped", mask_to_crop, 'fixed_image')
+
+    # binarize brain mask, not strictly necessary, but makes the workflow compatible with others
+    hard_post_brain_mask = Node(Threshold(threshold=kwargs['THRESHOLD']), name="hard_post_brain_mask")
+    hard_post_brain_mask.inputs.binarize = True
+    hard_post_brain_mask.inputs.clusterCheck = 'all'
+    workflow.connect(mask_to_crop, 'resampled_image', hard_post_brain_mask, 'img')
 
     # Intensity normalize co-registered image for tensorflow (ENDPOINT 1)
     img1_norm = Node(Normalization(percentile=kwargs['PERCENTILE']),
