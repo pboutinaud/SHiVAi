01c8437397f44df1ac888270c785d3d66ee6dfa7
Victor Nozais <vnozais@fealinx.com>
2024-03-25 10:52:29 +0100

    Improved print of licenses

--- src/shivautils/scripts/shiva.py
+++ src/shivautils/scripts/shiva.py
@@ -6,22 +6,21 @@ from shivautils.utils.shiva_runner import shiva
 
 def main():
     print(
-        '''
-        
-        SHiVAi segmentation pipeline.
-        Licences:
-        - The SHiVAi segmentation pipeline (source code and Apptainer image) is distributed under
-        the GNU General Public License - version 3 (GPLv3):
-        https://www.gnu.org/licenses/gpl-3.0.fr.html#license-text
-        - The SHiVAi trained AI models are distributed under and protected by the CC BY-NC-SA 4.0 license:
-        https://creativecommons.org/licenses/by-nc-sa/4.0/
-        
-        The segmentation models have been registered at the french 'Association de Protection des Programmes' under the numbers:
-        - For PVS: IDDN.FR.001.240015.000.S.P.2022.000.31230
-        - For WMH: IDDN.FR.001.240030.000.S.P.2022.000.31230
-        - For CMB: IDDN.FR.001.420007.000.S.P.2023.000.31230
-                          
-        '''
+        '\n'
+        'SHiVAi segmentation pipeline\n'
+        '\n'
+        'Licences:\n'
+        '- The SHiVAi segmentation pipeline (source code and Apptainer image) is distributed under'
+        'the GNU General Public License - version 3 (GPLv3):\n'
+        'https://www.gnu.org/licenses/gpl-3.0.fr.html#license-text \n'
+        '- The SHiVAi trained AI models are distributed under and protected by the CC BY-NC-SA 4.0 license:\n'
+        'https://creativecommons.org/licenses/by-nc-sa/4.0/ \n'
+        '\n'
+        "The segmentation models have been registered at the french 'Association de Protection des Programmes' under the numbers:\n"
+        '- For PVS: IDDN.FR.001.240015.000.S.P.2022.000.31230\n'
+        '- For WMH: IDDN.FR.001.240030.000.S.P.2022.000.31230\n'
+        '- For CMB: IDDN.FR.001.420007.000.S.P.2023.000.31230\n'
+        '\n'
     )
     parser = shivaParser()
     args = set_args_and_check(parser)
