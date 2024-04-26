a9fdf3f5452da90768becbf63f6d4dbba90f91e6
Victor Nozais <vnozais@fealinx.com>
2024-04-26 10:48:59 +0200

    Debuging

--- src/shivautils/workflows/dcm2nii_grafting.py
+++ src/shivautils/workflows/dcm2nii_grafting.py
@@ -3,6 +3,7 @@ from nipype.pipeline.engine import Node, Workflow
 from nipype.interfaces.dcm2nii import Dcm2niix
 from shivautils.interfaces.shiva import Dcm2niix_Singularity
 
+
 def graft_dcm2nii(workflow: Workflow, **kwargs) -> Workflow:
     """This function will interpose a dcm2nii node between the datagrabber
     and all the relevent connected nodes
@@ -17,28 +18,34 @@ def graft_dcm2nii(workflow: Workflow, **kwargs) -> Workflow:
     datagrabber = workflow.get_node('datagrabber')
     # List storing the reconnections to avoid doing it while iterating on the graph edges
     reconnections = []
+    dmc2nii_nodes = {}
     for _, connected_node, connection_dict in workflow._graph.out_edges(datagrabber, data=True):
         out_and_in = connection_dict['connect']
         for grab_out, node_in in out_and_in:
             if grab_out in ['img1', 'img2', 'img3']:  # Does not interpose dcm2nii for the "seg" output
-                if kwargs['CONTAINERIZE_NODES']:
-                    dcm2nii = Node(Dcm2niix_Singularity(), f'dicom2nifti_{grab_out}')
-                    dcm2nii.inputs.snglrt_bind = [
-                        (kwargs['BASE_DIR'], kwargs['BASE_DIR'], 'rw'),
-                        ('`pwd`', '`pwd`', 'rw'),]
-                    dcm2nii.inputs.snglrt_image = kwargs['CONTAINER_IMAGE']
-                else:
-                    dcm2nii = Node(Dcm2niix(), name=f'dicom2nifti_{grab_out}')
-                
-                dcm2nii.inputs.anon_bids = True
-                dcm2nii.inputs.out_filename = 'converted_%p'
-                reconnections.append((grab_out, dcm2nii, connected_node, node_in))
+                reconnections.append((grab_out, connected_node, node_in))
+                if grab_out not in dmc2nii_nodes.keys():
+                    if kwargs['CONTAINERIZE_NODES']:
+                        dcm2nii = Node(Dcm2niix_Singularity(), f'dicom2nifti_{grab_out}')
+                        dcm2nii.inputs.snglrt_bind = [
+                            (kwargs['BASE_DIR'], kwargs['BASE_DIR'], 'rw'),
+                            ('`pwd`', '`pwd`', 'rw'),]
+                        dcm2nii.inputs.snglrt_image = kwargs['CONTAINER_IMAGE']
+                    else:
+                        dcm2nii = Node(Dcm2niix(), name=f'dicom2nifti_{grab_out}')
+                    dcm2nii.inputs.anon_bids = True
+                    dcm2nii.inputs.out_filename = 'converted_%p'
+                    dmc2nii_nodes[grab_out] = dcm2nii
 
-    for grab_out, dcm2nii, connected_node, node_in in reconnections:
-        workflow.disconnect(datagrabber, grab_out, connected_node, node_in)
+    # dmc2nii_nodes specific loop because multiple node_in can use the same dmc2nii
+    for grab_out, dcm2nii in dmc2nii_nodes.items():
         workflow.connect(datagrabber, grab_out,
                          dcm2nii, 'source_dir')
-        workflow.connect(dcm2nii, 'converted_files',
+
+    for grab_out, connected_node, node_in in reconnections:
+        workflow.disconnect(datagrabber, grab_out,
+                            connected_node, node_in)
+        workflow.connect(dmc2nii_nodes[grab_out], 'converted_files',
                          connected_node, node_in)
-    
+
     return workflow
