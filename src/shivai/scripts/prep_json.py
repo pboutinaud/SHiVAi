fd7e56dc7ab7aa3e0bd5401a16df5d0ee78b4674
Victor Nozais <vnozais@fealinx.com>
2024-06-25 11:52:15 +0200

    reorganized prep_json to have an importable function (untested)

--- src/shivai/scripts/prep_json.py
+++ src/shivai/scripts/prep_json.py
@@ -58,6 +58,36 @@ def my_parser(targs, mods):
     return parser
 
 
+def json_maker(target, modalities, root_dir, model_files, version='', date='', author=''):
+    """Creates a json file used as a 'model descriptor' in Shivai
+
+    Args:
+        target (str): Type of segmentation done by the model ('PVS', 'WMH', 'CMB', 'LAC', 'brain_mask')
+        modalities (list): Imaging modalities used by the model (e.g. 't1', 'flair', 'swi', 't2', 't2s')
+        root_dir (str): Directory containing the model file
+        model_files (list): List of the model files (or directory), one per training fold (relative to 'root_dir')
+        version (str): (opt.) Version number of the model (usually also in the directory path)
+        date (str): (opt.) date of creation of the model
+        author (str): (opt.) Author of the model
+    """
+
+    model_dict = {}
+
+    # Preparing model_dict
+    model_dict['version'] = version
+    model_dict['release date'] = date
+    model_dict['author'] = author
+    model_dict['target'] = target
+    model_dict['modalities'] = modalities
+    model_files = [f[:-1] if f[-1] == os.sep else f for f in model_files]  # removing the "/" at the end if it's there
+    model_dir = os.path.dirname(model_files[0])
+    modelfiles_dicts = [{'name': f, 'md5': md5(os.path.join(root_dir, f))} for f in model_files]
+    model_dict['files'] = modelfiles_dicts
+    json_file = os.path.join(root_dir, model_dir, 'model_info.json')
+    with open(json_file, 'w') as fp:
+        json.dump(model_dict, fp, indent=2)
+
+
 def main():
 
     targets = ['PVS', 'WMH', 'CMB', 'LAC', 'brain_mask']
@@ -65,19 +95,14 @@ def main():
 
     parser = my_parser(targets, modalities)
     args = parser.parse_args()
-    model_dict = {}
 
     in_dir = os.path.abspath(args.folder)
     ext = args.extension
 
-    # Preparing model_dict
-    model_dict['version'] = args.version
-    model_dict['release date'] = args.date
-    model_dict['author'] = args.author
     # Auto detect target and modalities
     root_dir, dirname = os.path.split(in_dir)  # Name (not path) of the folder i.g. "T1.FLAIR-PVS")
     if args.target is not None:
-        model_dict['target'] = args.target
+        target = args.target
     else:
         targ = [t for t in targets if t in dirname]
         if 'brainmask' in dirname:
@@ -85,15 +110,15 @@ def main():
         if len(targ) != 1:
             raise ValueError(f'There should only be one target name in the folder name, but {len(targ)} were found: {targ}')
         else:
-            model_dict['target'] = targ[0]
+            target = targ[0]
     if args.modalities is not None:
-        model_dict['modalities'] = args.modalities
+        modals = args.modalities
     else:
         mod = [m for m in modalities if m.upper() in dirname]
         if len(mod) == 0:
             raise ValueError('No correct modality was found in the folder name')
         else:
-            model_dict['modalities'] = mod
+            modals = mod
 
     # Looking for the model files/folders
     if args.data_type == 'file':
@@ -120,11 +145,8 @@ def main():
         model_files = [f for f in model_files if os.path.isdir(os.path.join(root_dir, f))]  # keeping only the folders
     if not model_files:
         raise FileNotFoundError('No file/folder detected')
-    modelfiles_dicts = [{'name': f, 'md5': md5(os.path.join(root_dir, f))} for f in model_files]
-    model_dict['files'] = modelfiles_dicts
-    json_file = os.path.join(root_dir, model_dir, 'model_info.json')
-    with open(json_file, 'w') as fp:
-        json.dump(model_dict, fp, indent=2)
+
+    json_maker(target, modals, root_dir, model_files, args.version, args.date, args.author)
 
 
 if __name__ == "__main__":
