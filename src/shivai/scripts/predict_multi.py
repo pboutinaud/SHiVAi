cd67684240478fdf63b098c982e9943e4ea38b34
Victor Nozais <vnozais@fealinx.com>
2025-01-15 12:02:29 +0100

    predict_multi update for keras models, and using Path

--- src/shivai/scripts/predict_multi.py
+++ src/shivai/scripts/predict_multi.py
@@ -5,16 +5,17 @@
 # @author : Philippe Boutinaud - Fealinx
 
 import gc
-import os
 import json
+import sys
 
 import argparse
 from pathlib import Path
-
+import importlib.util as importu
+import tensorflow as tf
 
 import numpy as np
 import nibabel as nib
-import tensorflow as tf
+import keras
 from shivai.utils.misc import md5
 
 
@@ -112,26 +113,36 @@ def predict_parser():
 def main():
     pred_parser = predict_parser()
     args = pred_parser.parse_args()
-    if args.use_cpu:
-        tf.config.set_visible_devices([], 'GPU')
-        tf.config.threading.set_intra_op_parallelism_threads(args.use_cpu)
-        tf.config.threading.set_inter_op_parallelism_threads(args.use_cpu)
+    model_dir = args.model_dir  # type: Path
     # Obtaining the absolute path to all the model files
-    model_files = []
+    model_files = []  # type: list[Path]
     with open(args.descriptor) as f:
         meta_data = json.load(f)
     for mfile in meta_data['files']:
-        mdirname = os.path.basename(args.model_dir)
-        mfilename = mfile['name']
-        if mfilename[:len(mdirname)] == mdirname:  # model dir is in both args.model and mfile['name']
-            mfilename = os.path.join(*mfilename.split(os.sep)[1:])
-        model_file = os.path.join(args.model_dir, mfilename)
+        mfilename = Path(mfile['name'])
+        if not (model_dir / mfilename).exists():
+            if mfilename.parts[0] == model_dir.parts[-1]:
+                # model dir is in both model_dir and mfilename
+                model_dir = model_dir.parent
+        model_file = model_dir / mfilename
         model_files.append(model_file)
 
     notfound = []  # Doing it this way to found all the missing files in one pass
     badmd5 = []
+
+    keras_model = None
+    if 'script' in meta_data:
+        keras_model = model_dir / meta_data['script']['name']
+        if not keras_model.exists():
+            notfound.append(keras_model)
+        else:
+            k_md5 = meta_data['script']['md5']
+            k_hashmd5 = md5(keras_model)
+            if k_hashmd5 != k_md5:
+                badmd5.append(keras_model)
+
     for model_file, file_data in zip(model_files, meta_data['files']):
-        if not os.path.exists(model_file):
+        if not model_file.exists():
             notfound.append(model_file)
         else:
             hashmd5 = md5(model_file)
@@ -148,9 +159,9 @@ def main():
                          "Files in question:\n\t" +
                          "\n\t".join(badmd5))
 
-    # for modality in args.acq_types:
-    #     if modality not in meta_data['modalities']:
-    #         raise ValueError(f"ERROR : the prediction model does not use {modality} modality according to json descriptor file")
+    if keras_model:
+        # Execute keras_model to have access to its classes
+        exec(open(keras_model).read())
 
     # iterating over the model files and the input image files
     img1_files = args.img1_files
@@ -162,16 +173,32 @@ def main():
     if img2_files is not None and len(img1_files) != len(img2_files):
         raise ValueError('Missmatch between the number of main and secondary files')
     step = len(sub_list)//args.batch_size + int(bool(len(sub_list) % args.batch_size))
+
+    if args.use_cpu:
+        # if v:
+        # os.environ['CUDA_VISIBLE_DEVICES'] = ''
+        # from keras import backend as K
+        # K.set_session(K.tf.Session(config=K.tf.ConfigProto(intra_op_parallelism_threads=args.use_cpu,
+        #                                         inter_op_parallelism_threads=args.use_cpu)))
+        # else:
+        tf.config.set_visible_devices([], 'GPU')
+        tf.config.threading.set_intra_op_parallelism_threads(args.use_cpu)
+        tf.config.threading.set_inter_op_parallelism_threads(args.use_cpu)
+
     affine_dict = {}
     for fold, mfile in enumerate(model_files):
         # Load the model
-        tf.keras.backend.clear_session()
+        keras.backend.clear_session()
         gc.collect()
         print(f"Loading model file: {mfile}")
-        model = tf.keras.models.load_model(
-            mfile,
-            compile=False,
-            custom_objects={"tf": tf})
+        tmp_files = {}  # type: dict[str, Path]
+        if keras_model:
+            model = keras.saving.load_model(mfile, custom_objects=None, compile=False)
+        else:
+            model = keras.models.load_model(
+                mfile,
+                compile=False,
+                custom_objects={"tf": tf})
         for i in range(step):
             curr_slice = slice(i*args.batch_size, (i+1)*args.batch_size)
             input_images = np.zeros((len(sub_list[curr_slice]), *args.input_size, modality_num))
@@ -191,12 +218,14 @@ def main():
                 sub_pred = predictions[j].squeeze()
                 sub_pred[sub_pred < 0.001] = 0  # Threshold to remove near-zero voxels
                 subpred_im = nib.Nifti1Image(sub_pred.astype('float32'), affine=affine_dict[sub])
-                nib.save(subpred_im, f'tmp_{sub}_fold{fold}.nii.gz')
+                tmp_file = Path(f'tmp_{sub}_fold{fold}.nii.gz')
+                nib.save(subpred_im, tmp_file)
+                tmp_files[f'{sub}_{fold}'] = tmp_file
 
     # Taking each fold's results and averaging them
     print('Averaging the results of each model (done for each subject)...')
     for i, sub in enumerate(sub_list):
-        pred_list = [nib.load(f'tmp_{sub}_fold{fold}.nii.gz').get_fdata(dtype='float32') for fold in range(len(model_files))]
+        pred_list = [nib.load(tmp_files[f'{sub}_{fold}']).get_fdata(dtype='float32') for fold in range(len(model_files))]
         mean_pred = np.mean(pred_list, axis=0)
         if args.mask_files is not None:
             brainmask = nib.load(args.mask_files[sub_list.index(sub)]).get_fdata().astype(bool)
@@ -204,10 +233,10 @@ def main():
         mean_pred_im = nib.Nifti1Image(mean_pred.astype('float32'),  affine=affine_dict[sub])
         outname = args.foutname.format(sub=sub)
         if args.out_dir:
-            outname = os.path.join(args.out_dir, outname)
+            outname = args.out_dir / outname
         nib.save(mean_pred_im, outname)
         for fold in range(len(model_files)):
-            os.remove(f'tmp_{sub}_fold{fold}.nii.gz')
+            tmp_files[f'{sub}_{fold}'].unlink()
 
 
 if __name__ == "__main__":
