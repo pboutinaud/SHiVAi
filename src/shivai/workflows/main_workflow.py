b0fc6a19e3816352606fff568767202b9fec1b2a
Victor Nozais <vnozais@fealinx.com>
2026-09-08 15:07:12 +0200

    fix: same problem as before with databrager when using swomed. Patched.

--- src/shivai/workflows/main_workflow.py
+++ src/shivai/workflows/main_workflow.py
@@ -82,14 +82,15 @@ def update_wf_grabber(wf, acquisitions, datatype, kwargs, grabber_name='datagrab
     if datatype == 'dicom':
         graft_dcm2nii(wf, **kwargs)
 
-    # Clean up the datagrabber inputs to remove any empty fields (e.g. if no flair is provided)
-    # Specifically removes template_args default keys that are not in field_template
-    nok_fields = datagrabber.inputs.field_template.keys() ^ datagrabber.inputs.template_args.keys()
-    for field in nok_fields:
-        if field in datagrabber.inputs.field_template:
-            del datagrabber.inputs.field_template[field]
-        if field in datagrabber.inputs.template_args:
-            del datagrabber.inputs.template_args[field]
+    if data_struct != 'swomed':
+        # Clean up the datagrabber inputs to remove any empty fields (e.g. if no flair is provided)
+        # Specifically removes template_args default keys that are not in field_template
+        nok_fields = datagrabber.inputs.field_template.keys() ^ datagrabber.inputs.template_args.keys()
+        for field in nok_fields:
+            if field in datagrabber.inputs.field_template:
+                del datagrabber.inputs.field_template[field]
+            if field in datagrabber.inputs.template_args:
+                del datagrabber.inputs.template_args[field]
 
 
 def res_to_dict(sub_ids, in_files):
